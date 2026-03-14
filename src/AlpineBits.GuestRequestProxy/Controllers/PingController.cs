d8ca0fd9426cce6659433fd5637d20908e455b2f
Michael Zimmerhofer WORKS2023 <michaelzimmerhofer@outlook.com>
2026-03-14 08:25:18 +0100

    adding ping controller

---
+++ src/AlpineBits.GuestRequestProxy/Controllers/PingController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AlpineBits.GuestRequestProxy.Controllers
+{
+    [ApiController]
+    [Route("ping")]
+    public class PingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Pong");
+        }
+    }
+}
